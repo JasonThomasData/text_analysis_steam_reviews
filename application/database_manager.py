3b560a97a8736857fef8e10ab65e1526eea009a9
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-09-07 00:55:14 +1000

    The program now adds data to the DB.

--- application/database_manager.py
+++ application/database_manager.py
@@ -4,8 +4,14 @@ import sqlite3
 
 def create_reviews_table(db):
     cur = db.cursor()
-    cur.execute('CREATE TABLE IF NOT EXISTS steam_reviews (id serial PRIMARY KEY, url text, date_scraped text, user_recommendation text, user_review_text text, user_name text, user_review_date text);')
+    cur.execute('CREATE TABLE IF NOT EXISTS steam_reviews (id serial PRIMARY KEY, url text, date_scraped text, user_recommendation text, user_review_text text, user_review_date text);')
 
 def drop_reviews_table(db):
     cur = db.cursor()
     cur.execute('DROP TABLE steam_reviews;')
+
+def insert_data_reviews_table(db, url, date_scraped, user_recommendation, user_review_text, user_review_date):
+    cur = db.cursor()
+    query = "INSERT INTO steam_reviews (url, date_scraped, user_recommendation, user_review_text, user_review_date) VALUES (?,?,?,?,?);"
+    data = (url, date_scraped, user_recommendation, user_review_text, user_review_date)
+    cur.execute(query, data)
--- unit_test.py
+++ unit_test.py
@@ -28,5 +28,37 @@ class TestCreateDB(unittest.TestCase):
         with sqlite3.connect(db_location, timeout=20) as db:
             database_manager.drop_reviews_table(db)
 
+class TestInsertData(unittest.TestCase):
+    '''
+    Tests we can insert data into this db.
+    '''
+
+    def setUp(self):
+        db_location = 'database_test.db'
+        with sqlite3.connect(db_location, timeout=20) as db:
+            database_manager.create_reviews_table(db)
+
+    def test(self):
+        db_location = 'database_test.db'
+        with sqlite3.connect(db_location, timeout=20) as db:
+
+            url = 'url'
+            date_scraped = 'today'
+            user_recommendation = 'great'
+            user_review_text = 'great'
+            user_review_date = 'yesterday'
+            database_manager.insert_data_reviews_table(db, url, date_scraped, user_recommendation, user_review_text, user_review_date)
+
+            cur = db.cursor()
+            response = cur.execute("SELECT 1 FROM steam_reviews LIMIT 1;")
+            response_one_data = response.fetchone()
+            assert len(response_one_data) == 1
+
+    def tearDown(self):
+        db_location = 'database_test.db'
+        with sqlite3.connect(db_location, timeout=20) as db:
+            database_manager.drop_reviews_table(db)
+
+
 if __name__ == '__main__':
     unittest.main()
