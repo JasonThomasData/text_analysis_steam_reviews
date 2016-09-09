344a5d05720d81b42f7c300aac2c4f18f779deda
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-09-09 22:28:04 +1000

    This new function controls the scraper until it is interupted

--- application/scraper.py
+++ application/scraper.py
@@ -1,7 +1,8 @@
 #! usr/bin/env python3
 
-import requests
+import requests, sqlite3, datetime, time
 from bs4 import BeautifulSoup
+from application import database_manager
 
 
 def scrape_app_page(base_url, app_num):
@@ -98,3 +99,54 @@ def get_reviews_on_page(html_from_page):
 
     review_data_unique = remove_duplicates(review_data)
     return review_data_unique
+
+def get_reviews():
+    '''
+    The controlling function for the process that scrapes reviews from steam. Accessed from run_app.py
+    '''
+    sleep_time_between_requests = 1 #So Steam can't complain this is a burden on their scrapers.
+    scraper_increment = 5 #app_num increases this much every scraper request
+    start_scraping_app_num = 300000 #If the database contains no reviews, start with this app_num
+
+    db_location = 'database_steam_reviews.db'
+    db = sqlite3.connect(db_location, timeout=20)
+
+    database_manager.create_steam_reviews(db)
+
+    last_record = database_manager.retrieve_last_steam_reviews(db)
+
+    if last_record is None:
+        last_app_num = start_scraping_app_num
+    else:
+        last_app_num = last_record[2]
+
+    while(True):
+
+        """
+        This process of scraping Steam continues until it is disrupted.
+        """
+
+        time.sleep(sleep_time_between_requests)
+
+        last_app_num += scraper_increment
+
+        base_url = 'http://store.steampowered.com/app/'
+
+        content_from_steam = scrape_app_page(base_url, last_app_num)
+        date_scraped = datetime.datetime.now()
+
+        if page_has_reviews(content_from_steam) == True:
+            reviews_on_page = get_reviews_on_page(content_from_steam)
+            print('Found %s reviews for app number %s' %(len(reviews_on_page), last_app_num))
+        else:
+            reviews_on_page = []
+            print('No review element found for number %s' %(last_app_num))
+
+        for review in reviews_on_page:
+            url = '%s%s/' %(base_url, last_app_num)
+            classified = 0
+            user_recommendation = review['user_recommendation']
+            user_review_text = review['user_review_text']
+            user_name = review['user_name']
+
+            database_manager.insert_data_steam_reviews(db, url, last_app_num, date_scraped, classified, user_recommendation, user_review_text, user_name)
