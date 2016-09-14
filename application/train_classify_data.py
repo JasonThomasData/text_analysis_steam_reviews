79f6da0e2f1298b997ebe1fce40f1052c5715e18
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-09-14 23:11:29 +1000

    After some tests, this is disappointing, so this is going to be archived.

--- application/train_classify_data.py
+++ application/train_classify_data.py
@@ -1,5 +1,10 @@
 #! usr/bin/env python3
 
+'''
+For some reason, these classifiers have performed badly. I don't know why that is.
+I'm going to use the partial fit method with the MultinomialNB and save the instance 
+'''
+
 from application import data_prep
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,7 +16,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
 
-def train_classifier(classifier, training_vectors, training_classes):
+def train_mnb(training_vectors, training_classes):
     '''
     Trains a classifier and returns the result, so we can test it.
     '''
@@ -47,17 +52,32 @@ def train_logistic_regression(training_vectors, training_classes):
     classifier_logistic_regression.fit(training_vectors, training_classes)
     return classifier_logistic_regression
 
+
+def test_classifier(classifier, test_vectors, testing_classes, reviews_to_test):
+    '''
+    Tests a classifier with the testing data from the scraped reviews, which were retrieved from db.
+    '''
+
+    running_correct_number = 0
+    for i, _ignore in enumerate(testing_classes):
+        prediction = classifier.predict(test_vectors[i])
+
+        if prediction == testing_classes[i]:
+            running_correct_number += 1
+
+    return (running_correct_number / reviews_to_test) * 100
+
+
 def classify_reviews(db_location):
     '''
     This is the function to control this module, but it would take some time to run through the data, and I'm not sure how to test it.
     Our database has 5000 records we can test, so do that.
     '''
 
-    start_interval = 100
-    end_interval = 4900 #Put this in the run_app module, which is the user's interface.
-    reviews_to_test = 100
+    end_interval = 4500 #Put this in the run_app module, which is the user's interface.
+    reviews_to_test = 500
 
-    for reviews_to_train in range(start_interval, end_interval, reviews_to_test):
+    for reviews_to_train in range(reviews_to_test, end_interval, reviews_to_test):
 
         reviews_to_retrieve = reviews_to_train + reviews_to_test
 
@@ -67,9 +87,15 @@ def classify_reviews(db_location):
         training_vectors = vectorizer.fit_transform(training_documents)
         test_vectors = vectorizer.transform(testing_documents)
 
-        trained_mnd = train_mnd(training_vectors, training_classes)
+        trained_mnb = train_mnb(training_vectors, training_classes)
         trained_svc = train_svc(training_vectors, training_classes)
         trained_linear_svc = train_svc(training_vectors, training_classes)
         trained_logistic_regression = train_logistic_regression(training_vectors, training_classes)
 
-        print('Training classifiers with %s reviews, testing with %s reviews' %(reviews_to_train, reviews_to_test))
+        mnb_result = test_classifier(trained_mnb, test_vectors, testing_classes, reviews_to_test)
+        svc_result = test_classifier(trained_svc, test_vectors, testing_classes, reviews_to_test)
+        linear_svc_result = test_classifier(trained_linear_svc, test_vectors, testing_classes, reviews_to_test)
+        logistic_result = test_classifier(trained_logistic_regression, test_vectors, testing_classes, reviews_to_test)
+
+        result_string = '%.1f, %.1f, %.1f, %.1f' %(mnb_result, svc_result, linear_svc_result, logistic_result)
+        print(result_string)
