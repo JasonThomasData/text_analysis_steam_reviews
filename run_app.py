5dac08452e30285e2e9b96a9fb0dce20a90518c3
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-09-14 21:59:24 +1000

    Adding code that accesses train_classify_data module

--- run_app.py
+++ run_app.py
@@ -2,7 +2,7 @@
 
 import sys
 
-from application import scraper, database_manager
+from application import scraper, database_manager, train_classify_data
 
 if int(sys.version_info.major) < 3:
     sys.stdout.write('You must use Python3 with this program, exiting... \n')
@@ -14,8 +14,7 @@ def inputs_feedback():
     Wrong number of inputs. These are valid:
     - python3 run_app.py scrape_reviews continue OR
     - python3 run_app.py scrape_reviews new OR
-    - python3 run_app.py classify_data continue OR
-    - python3 run_app.py classify_data new OR
+    - python3 run_app.py classify_data OR
     - python3 run_app.py make_report OR
     '''
 
@@ -40,6 +39,9 @@ def process_inputs(inputs):
             scraper.get_reviews(db_location)
         else:
             return inputs_feedback()
+
+    elif inputs[1] == 'classify_data':
+        train_classify_data.classify_reviews(db_location)
 
     else:
         return inputs_feedback()
